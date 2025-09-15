e02f5f680ddca8740109278697c539c24d449453
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-09-15 15:09:16 +0700

    Update PS_N.cpp

--- 20250909/PS_N.cpp
+++ 20250909/PS_N.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <stack>
 
 using namespace std;
@@ -44,3 +43,52 @@ int main() {
     return 0;
 }
 
+
+// Cach khac 
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    freopen("PS.INP", "r", stdin);
+    freopen("PS.OUT", "w", stdout);
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    long long a[n + 1], mang[n + 1];
+    mang[0] = 0;
+    for (int i = 1; i <= n; ++i){
+        cin >> a[i];
+        mang[i] = mang[i - 1] + a[i];
+    }
+
+    int mins[n + 1];
+    mins[0] = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (mang[i] > mang[mins[i - 1]]) mins[i] = mins[i - 1];
+        else mins[i] = i;
+    }
+
+    long long maxval = 0;
+    int l = 1, r = 1;
+
+    int i = mins[n], j = n;
+    while (i >= 0) {
+        while (i < j && mang[j] <= mang[i]) --j;
+        if (j - i > maxval) {
+            maxval = j - i;
+            r = j;
+            l = i + 1;
+        }
+        if (i == 0) break;
+        i = mins[i - 1];
+    }
+
+    cout << l << " " << r << "\n";
+    return 0;
+}
