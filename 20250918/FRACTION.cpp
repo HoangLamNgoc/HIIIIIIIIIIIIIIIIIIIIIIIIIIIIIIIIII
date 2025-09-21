fd3d87b88a29d888dd18bde5999d261804abe48a
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-09-21 23:47:54 +0700

    Create FRACTION.cpp

---
+++ 20250918/FRACTION.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std; 
+
+int main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); cout.tie(0); 
+  
+    freopen("FRACTION.INP, "r", stdin); 
+    freopen("FRACTION.OUT, "w", stdout); 
+
+    int T; 
+    cin >> T; 
+
+    while (T--) {
+        int n; 
+        cin >> n; 
+      
+        long long a = 1, b = 1; 
+        for (int i = 1; i <= n; ++i ) {
+            long long j; cin >> j; 
+            long long g = gcd(a, j); 
+            a = a / g * j; 
+        }
+
+        for (int i = 1; i <= n; ++i ) {
+            long long j; cin >> j; 
+            long long g = gcd(b, j); 
+            b = b / g * j; 
+        }
+
+        long long k = gcd(a, b); 
+        a /= k; b/= k; 
+
+        while ( b % 2 == 0 ) b /= 2; 
+        while (b % 5 == 0 ) b /= 5;
+      
+        if ( b > 1 ) cout << "repeating" << endl;
+        else cout << "finite" << endl;
+    }
+return 0; 
+}
