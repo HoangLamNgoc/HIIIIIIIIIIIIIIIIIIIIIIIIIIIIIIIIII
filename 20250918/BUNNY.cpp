ca4954960de9ab5cbf637f021738a1aea838357e
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-09-22 10:49:25 +0700

    Update BUNNY.cpp

--- 20250918/BUNNY.cpp
+++ 20250918/BUNNY.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <algorithm>
 
-using namespace std; 
+using namespace std;
 const int maxN = 1e6;
 int main() {
     freopen("BUNNY.INP", "r", stdin);
-    freopen("BUNNY.OUT", "w", stdout); 
-    
-    long long n; 
-    cin >> n; 
+    freopen("BUNNY.OUT", "w", stdout);
 
-    long long k = n % 6; 
-    n /= 6; 
-    if ( k == 0 ) k = 1; 
-    int n = n % maxN; 
-    cout << k + n * ( n + 1 ) * 3 + n * k; 
+    long long n;
+    cin >> n;
 
-    return 0; 
+    long long k = n % 6;
+    n /= 6;
+    long long ans;
+    if ( k == 0 ) ans = 1 + (n % maxN) * (( n + 1) % maxN) * 3 + (n % maxN) * k;
+    else ans = k + (n % maxN) * (( n + 1) % maxN) * 3 + (n % maxN) * k;
+    cout << ans % maxN;
+    return 0;
 }
