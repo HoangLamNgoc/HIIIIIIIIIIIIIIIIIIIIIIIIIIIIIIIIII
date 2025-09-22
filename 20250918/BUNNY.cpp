5d39f44ceb827eaeb76e4e84a85fb923d5b65471
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-09-22 07:56:03 +0700

    Update BUNNY.cpp

--- 20250918/BUNNY.cpp
+++ 20250918/BUNNY.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main() {
     freopen("BUNNY.INP", "r", stdin);
     freopen("BUNNY.OUT", "w", stdout); 
-  
+    
     long long n; 
     cin >> n; 
 
     long long k = n % 6; 
     n /= 6; 
-
-    if ( k == 0 ) cout << 1 + k * (k + 1) * 3 + n * k; 
-    else cout << k + k * ( k + 1 ) * 3 + n * k; 
+    
+    if ( k == 0 ) cout << (1 + n * (n + 1) * 3 + n * k) % maxN; 
+    else cout << (k + n * ( n + 1 ) * 3 + n * k) % maxN; 
 
     return 0; 
 }
