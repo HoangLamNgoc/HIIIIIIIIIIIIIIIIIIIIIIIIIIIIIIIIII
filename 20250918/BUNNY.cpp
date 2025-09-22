79eac114a9439bc7fde8ff50320f4667fc46128b
HoangLamNgoc <ngochoanglam29102010@gmail.com>
2025-09-22 07:15:07 +0700

    Update BUNNY.cpp

--- 20250918/BUNNY.cpp
+++ 20250918/BUNNY.cpp
@@ -13,7 +13,7 @@ int main() {
     long long k = n % 6; 
     n /= 6; 
 
-    if ( k == 0 ) cout << 1 + k * ( k + 1 ) * 3 + n * k; 
+    if ( k == 0 ) cout << 1 + k * (k + 1) * 3 + n * k; 
     else cout << k + k * ( k + 1 ) * 3 + n * k; 
 
     return 0; 
